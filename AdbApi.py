16ddc8f640962c12e540d090405cce0f4b8b18d8
Joakim Johansson <mikaoj@gmail.com>
2018-12-02 09:54:17 +1100

    shell commands

--- AdbApi.py
+++ AdbApi.py
@@ -8,30 +8,26 @@ from adb import sign_m2crypto
 from adb import usb_exceptions
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 adb_api = Blueprint('adb_api', __name__)
 
-def run_commands(commands):
-   return [run_command(command) for command in commands]
-
-def run_command(command):
-  logging.debug('command=%s' % command)
+def run_shell_commands(shell_commands):
   try:
     signer = sign_m2crypto.M2CryptoSigner(os.path.expanduser('~/.android/adbkey'))
     device = adb_commands.AdbCommands()
     device.ConnectDevice(rsa_keys=[signer])
-    output = device.Shell(command["command"])
-    logging.debug('output=%s' % output)
-    command["output"] = output
-    return command
+    for command in shell_commands:
+      logging.info('shell=%s' % command['shell'])
+      logging.info('description=%s' % command['description'])
+      device.Shell(command['shell'])
   except Exception as ex:
     abort(500, str(ex))
 
 @adb_api.route('/shell', methods = ['POST'])
 def shell():
    data = request.get_json(force=True)
-   data['commands'] = run_commands(data["commands"])
+   data['sequence'] = run_shell_commands(data["sequence"])
    return json.dumps(data)
 
 @adb_api.route('/dumpsys', defaults={'thing': ""}, methods = ['GET'])
